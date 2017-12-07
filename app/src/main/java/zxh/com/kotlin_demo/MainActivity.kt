3cafa7b379f9667b90e1bda656a684ef1c4c96cf
15232842643 <15232842643@163.com>
2017-12-07 14:41:25 +0800

    anko布局

---
+++ app/src/main/java/zxh/com/kotlin_demo/MainActivity.kt
@@ -0,0 +1,27 @@
+package zxh.com.kotlin_demo
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        verticalLayout {
+            button("客官请点菜谱") {
+                textColor = Color.BLUE
+                textSize = 24.0f
+                onClick {
+                    val seleList = listOf("西红柿", "红烧肉", "土豆丝","微辣火锅","干锅酱鸭","干锅土豆片")
+                    selector("你想吃什么", seleList, { _, i ->
+                        toast("客官请稍等,您要的" + seleList[i]+"马上出锅")
+                    })
+                }
+            }
+        }
+    }
+
+}
